416da099af59eeccde9667775db284a0fafdaa89
singularity-sg <singularity.sg@gmail.com>
2012-03-24 15:55:44 +0800

    What a bummer... still can't crack it

--- src/kingdomconnectivity/Solution.java
+++ src/kingdomconnectivity/Solution.java
@@ -5,105 +5,121 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Solution {
 
 	InputStream is;
 	String[][] input = new String[2][];
-	String[] output;
-	
-	int N;
-	int M;
-	int[][] paths;
-	int count;
 
+	String[] output;
+
+	Integer N;
+	Integer M;
+	Integer[][] paths;
+	Integer[] nodeCount;
+
+	Integer count;
+	
+	private static final Integer INFINITE_LOOP = -999;
+	
 	public static void main(String[] args) throws Exception {
 		Solution kc = new Solution(System.in);
 		kc.go();
 		String[] output = kc.getOutput();
-		
-		for (String string : output) {			
+
+		for (String string : output) {
 			System.out.println(string);
 		}
 	}
-	
+
 	public Solution(InputStream in) {
 		this.is = in;
 		count = 0;
 	}
 
 	private void solve() {
-		
+
 		this.count = 0;
-		
+
 		LinkedList<Integer> visited = new LinkedList<Integer>();
 		visited.add(1);
+
+		this.count = oneMoreIdea(paths, 1, false);
 		
-		breadthFirst(paths, visited, false);
-		
-		if(count > -1) {
-			output = new String[] {String.valueOf(count)};
+		if (!count.equals(INFINITE_LOOP)) {
+			output = new String[] { String.valueOf(count % 1000000000) };
 		} else {
-			output = new String[] {"INFINITE PATHS"};
+			output = new String[] { "INFINITE PATHS" };
 		}
-	
+
 	}
 	
-	private void breadthFirst(int[][] graph, LinkedList<Integer> visited, boolean repeatedBefore) {
-		int lastVisited = 1;
-		boolean repeat = repeatedBefore;
+	private int oneMoreIdea(Integer[][] graph, Integer node, boolean looped) {
 		
-		if(visited.size() > 0) {
-			lastVisited = visited.getLast();
-		}
+		Integer[] adjacentNodes = graph[node-1];
+		int count = 0;
+		boolean loop = looped; 
 		
-		int[] adjacentNodes = graph[lastVisited-1];
-		
-		if(adjacentNodes != null) {
-		
-			for(int i=0;i<adjacentNodes.length;i++) {
-				if(!containsNode(adjacentNodes[i], visited)) {
-					visited.add(adjacentNodes[i]);
-					if(adjacentNodes[i] == this.N) {
-						this.count++;
-						if(repeat) {
-							count = -1;
-							printVisited(visited, repeat);
-							return;
+		if(node.intValue() == this.N.intValue()) {
+			if(loop) {
+				return INFINITE_LOOP; //INFINITE LOOP
+			} else {
+				count = 1;
+			}
+		} else {
+			if(adjacentNodes != null) {
+				
+				if(this.nodeCount[node-1] == -1) {
+					
+					Map<Integer, Integer> adjacentNodesMap = new HashMap<Integer, Integer>();
+
+					for(int i=0;i<adjacentNodes.length;i++) {
+						Integer curNode = adjacentNodes[i];
+						Integer curCount = adjacentNodesMap.get(curNode);
+						if(curCount != null) {
+							adjacentNodesMap.put(curNode, ++curCount);
+						} else {
+							adjacentNodesMap.put(curNode, 1);
 						}
-						printVisited(visited, repeat);
-					} else {
-						breadthFirst(graph, visited, repeat);
 					}
-					visited.removeLast();
+					
+					Set<Entry<Integer,Integer>> set = adjacentNodesMap.entrySet();
+					Iterator<Entry<Integer,Integer>> it = set.iterator();
+					
+					while(it.hasNext()) {
+						
+						Entry<Integer, Integer> cur = it.next();
+						
+						if(cur.getKey() < node) {
+							loop = true;
+							continue;
+						}
+						
+						int curCount = oneMoreIdea(graph, cur.getKey(), loop);
+						
+						if(curCount != INFINITE_LOOP) {
+							count += curCount * cur.getValue();
+						} else {
+							return curCount;
+						}
+						
+					}
+					
+					this.nodeCount[node-1] = count;
 				} else {
-					repeat = true;
+					count = this.nodeCount[node-1];
 				}
 			}
 		}
-    }
-	
-	private void printVisited(List<Integer> visited, boolean repeat) {
-		Iterator<Integer> it = visited.iterator();
-		while(it.hasNext()) {
-			System.out.print(it.next() + " ");
-		}
-		System.out.print(", Repeat="+repeat);
-		System.out.println("\n");
-	}
-
-	private boolean containsNode(int node, List<Integer> visited) {
 		
-		for (Integer visitedNode : visited) {
-			if(node == visitedNode.intValue()) {
-				return true;
-			}
-		}
-		
-		return false;
+		return count;
 	}
 
 	public void go() throws Exception {
@@ -116,59 +132,49 @@ public class Solution {
 		String[] lineOne = this.input[0][0].split(" ");
 		this.N = Integer.valueOf(lineOne[0]);
 		this.M = Integer.valueOf(lineOne[1]);
-		this.paths = new int[N][];
-			
-		Arrays.sort(this.input[1]);
+		this.paths = new Integer[N][];
+		this.nodeCount = new Integer[N];
 		
+		Arrays.fill(this.nodeCount, -1);
+		Arrays.sort(this.input[1]);
+
 		List<Integer> currentCityPaths = new ArrayList<Integer>();
 		int prevCity = -1;
-		
-		for(int i=0;i<M;i++) {
-			
+
+		for (int i = 0; i < M; i++) {
+
 			String[] line = this.input[1][i].split(" ");
 			int x = Integer.valueOf(line[0]);
 			int y = Integer.valueOf(line[1]);
-			
-			if(x == prevCity || prevCity == -1) {
+
+			if (x == prevCity || prevCity == -1) {
 				currentCityPaths.add(y);
 			} else {
-				this.paths[prevCity-1] = convertIntegerToIntArray(currentCityPaths);
+				this.paths[prevCity - 1] = currentCityPaths
+						.toArray(new Integer[] {});
 				currentCityPaths = new ArrayList<Integer>();
 				currentCityPaths.add(y);
 			}
-			
+
 			prevCity = x;
 		}
-		
-		this.paths[prevCity-1] = convertIntegerToIntArray(currentCityPaths); 
-		
-	}
 
-	private int[] convertIntegerToIntArray(List<Integer> currentCityPaths) {
-		
-		int[] arr = new int[currentCityPaths.size()];
-		
-		Iterator<Integer> it = currentCityPaths.iterator();
-		int i = 0;
-		while(it.hasNext()) {
-			arr[i] = it.next();
-			i++;
-		}
-		
-		return arr;
+		this.paths[prevCity - 1] = currentCityPaths.toArray(new Integer[] {});
+
 	}
 
 	private void readInput() throws Exception {
 		List<String> lines = new ArrayList<String>();
 		String line;
-		BufferedReader reader = new BufferedReader(new InputStreamReader(this.is));
-		if((line = reader.readLine()) != null) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				this.is));
+		if ((line = reader.readLine()) != null) {
 			this.input[0] = new String[] { line };
 		}
-		while((line = reader.readLine()) != null) {
+		while ((line = reader.readLine()) != null) {
 			lines.add(line);
 		}
-		
+
 		reader.close();
 		this.input[1] = lines.toArray(new String[] {});
 	}
@@ -176,5 +182,5 @@ public class Solution {
 	public String[] getOutput() {
 		return output;
 	}
-	
+
 }
--- test/kingdomconnectivity/TestKingdomConnectivity.java
+++ test/kingdomconnectivity/TestKingdomConnectivity.java
@@ -1,6 +1,7 @@
 package kingdomconnectivity;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import org.junit.Test;
 public class TestKingdomConnectivity {
 
 	@Test
-	public void testCaseOne() throws Exception {
+	public void testCaseZero() throws Exception {
 		InputStream input = TestKingdomConnectivity.class.getResourceAsStream("input00.txt");
 			
 		Solution kc = new Solution(input);
@@ -24,7 +25,7 @@ public class TestKingdomConnectivity {
 	}
 	
 	@Test
-	public void testCaseTwo() throws Exception {
+	public void testCaseOne() throws Exception {
 		InputStream input = TestKingdomConnectivity.class.getResourceAsStream("input01.txt");
 			
 		Solution kc = new Solution(input);
@@ -36,7 +37,7 @@ public class TestKingdomConnectivity {
 	}
 	
 	@Test
-	public void testCaseThree() throws Exception {
+	public void testCaseTwo() throws Exception {
 		InputStream input = Solution.class.getResourceAsStream("input02.txt");
 			
 		Solution kc = new Solution(input);
@@ -48,7 +49,7 @@ public class TestKingdomConnectivity {
 	}
 	
 	@Test
-	public void testCaseFour() throws Exception {
+	public void testCaseThree() throws Exception {
 		
 		long start = System.currentTimeMillis();
 		
@@ -63,6 +64,69 @@ public class TestKingdomConnectivity {
 		String[] output = getOutput("output03.txt");
 		
 		Assert.assertArrayEquals(output, kc.getOutput());
+	}
+	
+	@Test
+	public void testCaseFour() throws Exception {
+		
+		long start = System.currentTimeMillis();
+		
+		InputStream input = Solution.class.getResourceAsStream("input04.txt");
+			
+		Solution kc = new Solution(input);
+		kc.go();
+		
+		long time = System.currentTimeMillis() - start;
+		System.out.println("Time :" + time + "ms");
+		
+		String[] output = getOutput("output04.txt");
+		
+		Assert.assertArrayEquals(output, kc.getOutput());
+	}
+	
+	@Test
+	public void testCaseFive() throws Exception {
+		
+		long start = System.currentTimeMillis();
+		
+		InputStream input = Solution.class.getResourceAsStream("input05.txt");
+			
+		Solution kc = new Solution(input);
+		kc.go();
+		
+		long time = System.currentTimeMillis() - start;
+		System.out.println("Time :" + time + "ms");
+		
+		String[] output = getOutput("output05.txt");
+		
+		Assert.assertArrayEquals(output, kc.getOutput());
+	}
+	
+	@Test
+	public void testCaseSix() throws Exception {
+		StringBuilder test = new StringBuilder();
+		test.append("1000 999\n");
+		int cnt = 0;
+		
+		for(int i=1;i<=999;i++) {
+			for(int j=i+1;j<=(i+1) && j<=1000;j++) {
+				test.append(i + " " + j + "\n");
+				cnt++;
+			}
+		}
+		
+		InputStream is = new ByteArrayInputStream(test.toString().getBytes());
+		
+		long start = System.currentTimeMillis();
+		
+		Solution kc = new Solution(is);
+		kc.go();
+		
+		long time = System.currentTimeMillis() - start;
+		System.out.println("Time :" + time + "ms");
+		
+		Assert.assertArrayEquals(new String[]{"1"}, kc.getOutput());
+	
 	}
 
 
